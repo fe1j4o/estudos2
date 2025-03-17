8907f2ea984c62880e34ecef08a25b024b00c893
fe1j4o <98670763+fe1j4o@users.noreply.github.com>
2025-03-17 19:36:04 -0400

    Add files via upload

---
+++ L2_poligonos.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.141592653589793
+
+int main(){
+	float comprimento, area;
+	int lados;
+	printf("escolha qual forma voce deseja saber a area digitando o seu numero de lados (3, 4 ou 5): ");
+	scanf("%d", &lados);
+    printf("digite o comprimento dos lados da forma: ");
+	scanf("%f", &comprimento);
+	if(lados==3){
+        area = (comprimento * comprimento *sqrt(3)) / 4;
+	    printf("a area do triangulo é %f", area);
+	}
+	else if(lados==4){
+		area = (comprimento * comprimento);
+		printf("a area do quadrado é %f", area);
+	}
+	else if(lados==5){
+		area = (5 * comprimento * comprimento) / (4 * tan(PI / 5));
+		printf("a area do pentagono é %f", area);
+	}
+	else{
+		printf("por favor digite 3, 4 ou 5");
+	}
+	return 0;
+}
